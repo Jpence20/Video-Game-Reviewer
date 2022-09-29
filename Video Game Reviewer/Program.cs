f53af83348ca0f7df5fbff76658ac8fafb0391af
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-09-28 20:04:29 -0400

    update

--- Video Game Reviewer/Program.cs
+++ Video Game Reviewer/Program.cs
@@ -1,6 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System;
+
+namespace videoGameReviewer
+{
+   public class program 
+    {
+        public static String choice;
+        static void Main(String[] args)
+        {
+            selection();
+           
+        }
+
+       public static void selection()
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine("*      Video Game Reviews    *");
+            Console.WriteLine("******************************");
+            Console.WriteLine("Choose if you want to see a review or write a review");
+            Console.WriteLine("1.See Review");
+            Console.WriteLine("2.Write Review");
+            Console.WriteLine("3.Exit");
+            String choice = Console.ReadLine();
+            ChoiceSelection(choice);
+
+
+
+        }
+
+        public static void ChoiceSelection(string choice)
+        {
+            switch (choice)
+            {
+                case "1":
+                    {
+                        
+                        Console.WriteLine($"See Review");
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Write Review");
+                        break;
+                    }
+                case "3":
+                    {
+                        break;
+                    }
+                default:
+                    {
+
+                        break;
+                    }
+            }
+        }
+        public static void Case1()
+        {
+            Console.WriteLine("See Review");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 
 
 
