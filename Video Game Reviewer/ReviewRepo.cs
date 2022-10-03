09541e82faaca6c580a53130b5f714f2b2a6d2d9
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-10-03 18:26:34 -0400

    update

--- Video Game Reviewer/ReviewRepo.cs
+++ Video Game Reviewer/ReviewRepo.cs
@@ -6,29 +6,37 @@ public class ReviewRepo
     {
         reviews godOfWar = new reviews("Jeremy Pence", "God of war", "Great Game", 40, 10);
 
-        var reviews = new Dictionary<string, reviews>();
-        reviews.Add("god of war", godOfWar);
+        var reviews = new Dictionary<string, reviews>
+        {
+            { "god of war", godOfWar }
+        };
         return reviews;
     }
     public static Dictionary<string, reviews> InitializeReviewsA()
     {
         reviews godOfWar = new reviews("John Pence", "God of war", "Great Game", 40, 10);
-        var reviews = new Dictionary<string, reviews>();
-        reviews.Add("god of war", godOfWar);
+        var reviews = new Dictionary<string, reviews>
+        {
+            { "god of war", godOfWar }
+        };
         return reviews;
     }
     public static Dictionary<string, reviews> InitializeReviewsB()
     {
         reviews godOfWar = new reviews("Jessica Pence", "God of war", "Okay Game", 40, 8);
-        var reviews = new Dictionary<string, reviews>();
-        reviews.Add("god of war", godOfWar);
+        var reviews = new Dictionary<string, reviews>
+        {
+            { "god of war", godOfWar }
+        };
         return reviews;
     }
     public static Dictionary<string, reviews> InitializeReviewsC()
     {
         reviews godOfWar = new reviews("Brian Maggard", "God of war", "Great Game", 60, 10);
-        var reviews = new Dictionary<string, reviews>();
-        reviews.Add("god of war", godOfWar);
+        var reviews = new Dictionary<string, reviews>
+        {
+            { "god of war", godOfWar }
+        };
         return reviews;
     }
 
