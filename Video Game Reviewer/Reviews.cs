cbb67e74038608ff4ceca0b51acdce63a029d0f0
Jpence20 <89040154+Jpence20@users.noreply.github.com>
2022-11-08 18:56:20 -0500

    Extracted WrittenReviews class to its own file

--- Video Game Reviewer/Reviews.cs
+++ Video Game Reviewer/Reviews.cs
@@ -20,15 +20,5 @@
     }
 
 
-    public class WrittenReviews : Reviews
-    {
-        public string DateWritten { get; set; }
-
-        public WrittenReviews(string reviewer, string game, string review, string system, double score,string dateWritten) : base(reviewer, game, review, system, score)
-        {
-            this.DateWritten = dateWritten;
-
-        }
-        public override string ToString() => $"\r\nReviewed by {Reviewer}:\r\nGame: {Game}\r\nReview: {Review}\r\nReviewed on:{system}\r\nReview Score:{Score}\r\n Date:{DateWritten}";
-    }
+   
 }
---
+++ Video Game Reviewer/WrittenReviewsClass.cs
@@ -0,0 +1,14 @@
+﻿namespace Video_Game_Reviewer
+{
+    public class WrittenReviews : Reviews
+    {
+        public string DateWritten { get; set; }
+
+        public WrittenReviews(string reviewer, string game, string review, string system, double score, string dateWritten) : base(reviewer, game, review, system, score)
+        {
+            this.DateWritten = dateWritten;
+
+        }
+        public override string ToString() => $"\r\nReviewed by {Reviewer}:\r\nGame: {Game}\r\nReview: {Review}\r\nReviewed on:{system}\r\nReview Score:{Score}\r\n Date:{DateWritten}";
+    }
+}
